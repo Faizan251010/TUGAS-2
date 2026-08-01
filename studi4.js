13b96131b0d1502efa838e8d0e3c4cd1948884a8
Faizan Farzani <afaizan.2510@gmail.com>
2026-08-01 21:25:18 +0800

    p

---
+++ studi4.js
@@ -0,0 +1,18 @@
+function evaluasiAsupanKalori(kalori) {
+    let totalKalori = 0;
+    let target = '';
+
+    for (let i = 0; i < kalori.length; i++) {
+        totalKalori += kalori[i];
+    } if (totalKalori == 3000) {
+        target = 'Kalori sudah Pas';
+    } else if (totalKalori > 3000) {
+        target = 'Kalori lebih dari target, kurangi kalori';
+    } else {
+        target = 'Kalori kurang dari target, tambah kalori';
+    }
+    console.log (`Asupan kalori ${totalKalori} kalori ${target}`)
+}
+
+const kalori = [220, 480, 350, 400, 950, 800]
+evaluasiAsupanKalori(kalori);
